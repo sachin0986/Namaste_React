9d9dce9358ac60a9c56dad2b34444317fa04a7a3
Sachin Arora <sachinarora0986@gmail.com>
2024-08-07 22:14:52 +0530

    real time api added to the cards

--- src/components/CenterBody.jsx
+++ src/components/CenterBody.jsx
@@ -1,31 +1,29 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ResturantCard from './ResturantCard';
 import { useState, useEffect } from 'react';
-import resList from '../utils/mockData';
-import { Data_URL } from '../utils/constants';
 import { LuSearch } from "react-icons/lu";
 
 const CenterBody = () => {
-    const [RestaurantsData, setRestaurantsData] = useState(resList);
-    const [Search, setSearch] = useState(''); //we need to make the current state of search is = empty = useState(''); --> Like that otherwise error of can't read the properties of toLoweCase() function.
-    // useEffect(() => {
-    //     fetchData();
-    // }, [])
+    const [RestaurantsData, setRestaurantsData] = useState([]);
+     //we need to make the current state of search is = empty = useState(''); --> Like that otherwise error of can't read the properties of toLoweCase() function.
+    useEffect(() => {fetchData()}, []);
     //Important very.....
 
-    // const fetchData = async () => {
-    //    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=29.22110&lng=78.96060"); //use async await for promises
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=12.9715987&lng=77.5945627"); //use async await for promises
      
-    // const json = await data.json(); //converting into json format
-    // console.log(json);
-    // };
+        const json = await data.json(); //converting into json format
+        console.log(json?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurantsData(json?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        };
 
     return(
         <div className="body">
             <div className="shot_head">
                 {/*Search Button*/}
             <div className="searchButton">
-                <input type="text" onChange={(e) => setSearch(e.target.value)} placeholder='Search For Restaurants'/>
+                <input type="text" placeholder='Search For Restaurants'/>
                 <button>< LuSearch /></button>
             </div>
                      {/*Filter Button*/}
@@ -37,19 +35,12 @@ const CenterBody = () => {
                 }}>Top Rated Resturants</button>
             </div>
                  {/*All Restaurents Button*/}
-            <div className="allrest">
-                <button className='All_rest' onClick={() => {
-                    setRestaurantsData(resList);
-                }}>All Restaurents</button>
-            </div>
             </div>
             <div className="Cart">
                 {/*if something is reused again again then create a Seprate Component */}
             {/*map*/}
             {
-                RestaurantsData.filter((resturant) => {
-                    return Search.toLowerCase() === '' ? resturant : resturant.info.name.toLowerCase().includes(Search)
-                }).map((resturant) => 
+                RestaurantsData.map((resturant) => 
                     (<ResturantCard key={resturant.info.id} resData={resturant}/> ))
             }
             {/* unique Key={} whenever you use map function or loop in react always use key{} property why? - it provode a uinque id to 
